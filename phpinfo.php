667b0ddea63bece39cc15847d70bf6b30fdc8b93
Andraina Ho Kiady <ahkiady@pm.me>
2023-10-19 02:49:18 +0200

    Configuration PHP et Apache

---
+++ phpinfo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Test</title>
+    </head>
+    <body>
+        <?php
+            phpinfo();
+            echo "Hello, php is working !";
+        ?>
+
+    </body>
+</html>
